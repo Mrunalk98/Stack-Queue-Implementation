787595910a1d11c43271352d9eb25ee7d3b547e3
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-21 20:45:31 +0530

    [mrunal] Add. Created a stack and added push and display methods

--- StackQueueImplementation/Program.cs
+++ StackQueueImplementation/Program.cs
@@ -7,6 +7,11 @@ namespace StackQueueImplementation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Queue and Stack Implementation !");
+            Stack stack = new Stack();
+            stack.Push(56);
+            stack.Push(30);
+            stack.Push(70);
+            stack.Display();
         }
     }
 }
---
+++ StackQueueImplementation/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackQueueImplementation
+{
+    public class Stack
+    {
+        internal Node top;
+
+        public Stack()
+        {
+            this.top = null;
+        }
+
+        internal void Push(int data)
+        {
+            Node node = new Node(data);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine(data + " inserted into Stack");
+        }
+
+        internal void Display()
+        {
+            Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            Console.WriteLine("\nData present in Stack : ");
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+
+    }
+}
