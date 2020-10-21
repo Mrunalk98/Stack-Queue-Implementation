8a2f36ddf926eab74df6988639246a3df134d954
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-21 20:26:11 +0530

    [mrunal] Add. Added welcom message

---
+++ StackQueueImplementation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackQueueImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Queue and Stack Implementation !");
+        }
+    }
+}
